37d23d907497fa8b50e5d18c4dac43e9b25be73a
decisionwheel <rebeccanobelgirl@gmail.com>
2026-05-22 23:49:39 +0500

    Update footer.js

--- footer.js
+++ footer.js
@@ -43,16 +43,15 @@
             <ul>
               <li><a href="#faq">FAQ</a></li>
               <li><a href="#tips">Tips & Tricks</a></li>
-              <li><a href="#templates">Templates</a></li>
-              <li><a href="#about">About</a></li>
+              <li><a href="#templates">Templates</a></li>              
             </ul>
           </div>
           <div class="footer-col">
             <h4>Legal</h4>
             <ul>
               <li><a href="/privacy">Privacy Policy</a></li>
-              <li><a href="/terms">Terms of Service</a></li>
-              <li><a href="/cookies">Cookie Policy</a></li>
+              <li><a href="/terms">Terms of Use</a></li>
+              <li><a href="/about">About</a></li>
               <li><a href="/contact">Contact</a></li>
             </ul>
           </div>
